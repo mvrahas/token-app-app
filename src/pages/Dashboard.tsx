f5ffe0e78aef25340f6873748c87d64a93721e35
Mark Vrahas <mvrahas@users.noreply.github.com>
2025-04-30 18:57:10 -0700

    display usdc balance on dash

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,8 +1,27 @@
+import { useState,useEffect } from "react"
+import api from "../functions/api"
+
+
 const Dashboard = ()=>{
+
+
+    //get account balance
+    const [balance,setBalance] = useState(null)
+    const getBalance = ()=>{
+        api.get(`/organization/balance`)
+        .then(r=>setBalance(r.data.uiAmount))
+        .catch(e=>console.log(e))
+    }
+    useEffect(getBalance,[])
+
+
     return(
         <div>
-            This is the dashboard. HOME
+            <p>USDC Balance</p>
+            <span>{balance}</span>
         </div>
     )
 }
+
+
 export default Dashboard
