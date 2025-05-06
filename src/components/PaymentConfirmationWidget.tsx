b83db8fccb8012c2e0091930a2e72fff105c0214
Mark Vrahas <mvrahas@users.noreply.github.com>
2025-05-06 10:57:02 -0700

    open return url

--- src/components/PaymentConfirmationWidget.tsx
+++ src/components/PaymentConfirmationWidget.tsx
@@ -32,7 +32,7 @@ const PaymentConfirmationWidget : React.FC<PaymentConfirmationWidgetProps> = ({i
 
                     <div className="w-full mt-2">
                         <button 
-                            onClick={()=>{console.log('hey')}}
+                            onClick={()=>window.location.href=info.returnURL}
                             className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer"
                         >
                             Back to website
