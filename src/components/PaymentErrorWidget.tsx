e467dcce2ec69a752f5d0090e4391fb3864066a2
Mark Vrahas <mvrahas@users.noreply.github.com>
2025-05-06 11:30:01 -0700

    add mailto link to error widget

--- src/components/PaymentErrorWidget.tsx
+++ src/components/PaymentErrorWidget.tsx
@@ -32,7 +32,7 @@ const PaymentErrorWidget : React.FC<PaymentErrorWidgetProps> = ({info})=>{
 
                     <div className="w-full mt-2">
                         <button 
-                            onClick={()=>{console.log('hey')}}
+                            onClick={()=>window.location.href='mailto:support@numin.xyz'}
                             className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer"
                         >
                             Get help
