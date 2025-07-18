fad6d1f110f6c5c4f6ef39651ebeb9740a992c91
Mark Vrahas <mvrahas@users.noreply.github.com>
2025-07-18 16:41:39 -0700

    send success/fail state

--- src/pages/TransferReceive.tsx
+++ src/pages/TransferReceive.tsx
@@ -43,67 +43,72 @@ const TransferReceive = ()=>{
 
     return (
         <form onSubmit={receive}>
-          <div className="space-y-12">
+            <div className="space-y-12">
 
 
 
-            <div className="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-gray-900/10 pb-12 md:grid-cols-3">
-              <div>
-                <h2 className="text-base/7 font-semibold text-gray-900">Receive</h2>
-                <p className="mt-1 text-sm/6 text-gray-600">
-                    Create a unique link that your customers can use to complete a payment.
-                </p>
-              </div>
-              <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 md:col-span-2">
+                <div className="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-gray-900/10 pb-12 md:grid-cols-3">
 
 
-                <div className="sm:col-span-4">
-                    <label htmlFor="amount" className="block text-sm/6 font-medium text-gray-900">
-                        Amount:
-                    </label>
-                    <div className="mt-2">
-                        <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                            <input
-                                type="number"
-                                id="amount"
-                                name="amount"
-                                placeholder="100"
-                                value={amount === 0 ? '' : amount}
-                                onChange={handleInput}
-                                className="uppercase block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
-                                required
-                            />
+
+                    <div>
+                        <h2 className="text-base/7 font-semibold text-gray-900">Receive</h2>
+                        <p className="mt-1 text-sm/6 text-gray-600">
+                            Create a unique link that your customers can use to complete a payment.
+                        </p>
+                    </div>
+
+
+
+                    <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 md:col-span-2">
+
+                        <div className="sm:col-span-4">
+                            <label htmlFor="amount" className="block text-sm/6 font-medium text-gray-900">
+                                Amount:
+                            </label>
+                            <div className="mt-2">
+                                <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                                    <input
+                                        type="number"
+                                        id="amount"
+                                        name="amount"
+                                        placeholder="100"
+                                        value={amount === 0 ? '' : amount}
+                                        onChange={handleInput}
+                                        className="uppercase block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                                        required
+                                    />
+                                </div>
+                            </div>
                         </div>
+
+                        <div className="sm:col-span-4">
+                            <label htmlFor="memo" className="block text-sm/6 font-medium text-gray-900">
+                                Memo:
+                            </label>
+                            <div className="mt-2">
+                                <textarea
+                                    id="memo"
+                                    name="memo"
+                                    rows={3}
+                                    placeholder="Digital media licensing fees for Brain Farm Creative"
+                                    value={memo}
+                                    onChange={(e:ChangeEvent<HTMLTextAreaElement>)=>{setMemo(e.target.value)}}
+                                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                    required
+                                />
+                            </div>
+                        </div>
+
                     </div>
+
+
+
                 </div>
-
-
-                <div className="sm:col-span-4">
-                    <label htmlFor="memo" className="block text-sm/6 font-medium text-gray-900">
-                        Memo:
-                    </label>
-                    <div className="mt-2">
-                        <textarea
-                            id="memo"
-                            name="memo"
-                            rows={3}
-                            placeholder="Digital media licensing fees for Brain Farm Creative"
-                            value={memo}
-                            onChange={(e:ChangeEvent<HTMLTextAreaElement>)=>{setMemo(e.target.value)}}
-                            className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                            required
-                        />
-                    </div>
-                </div>
-
-
-
-              </div>
-            </div>
 
 
             
-          </div>
+            </div>
     
 
 
@@ -121,9 +126,7 @@ const TransferReceive = ()=>{
                     </button>
                 </div>
             }
-
-          
-          {error ? <p className="mt-6 flex items-center justify-end gap-x-6 text-red-400">{error}</p> : null}
+            {error ? <p className="mt-6 flex items-center justify-end gap-x-6 text-red-400">{error}</p> : null}
 
 
 
--- src/pages/TransferSend.tsx
+++ src/pages/TransferSend.tsx
@@ -9,6 +9,7 @@ const TransferSend = ()=>{
     const navigate = useNavigate()
     const [loading,setLoading] = useState(false)
     const [error,setError] = useState('')
+    const [signature,setSignature] = useState('')
 
 
     const [to,setTo] = useState('')
@@ -23,13 +24,18 @@ const TransferSend = ()=>{
         try{
 
           //api call to create mint
-          await api.post('/mint',{})
+          const {data} = await api.post('/transaction/send',{
+            to,
+            amount,
+            memo,
+          })
             
           //ok->
-          navigate(`/mint`)
+          setSignature(data.signature)
 
-        }catch(e){
-          setError('Oops! Something went wrong. Please try again.')
+        }catch(e:any){
+            if(e.response.data.error){setError(e.response.data.error)}
+            else{setError('Oops! Something went wrong!')}
         }
         setLoading(false)
     }
@@ -43,100 +49,112 @@ const TransferSend = ()=>{
 
     return (
         <form onSubmit={send}>
-          <div className="space-y-12">
+        <div className="space-y-12">
 
 
 
             <div className="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-gray-900/10 pb-12 md:grid-cols-3">
-              <div>
-                <h2 className="text-base/7 font-semibold text-gray-900">Send</h2>
-                <p className="mt-1 text-sm/6 text-gray-600">
-                  Instantly transfer funds to another wallet in the same network.
-                </p>
-              </div>
-              <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 md:col-span-2">
 
 
-                <div className="sm:col-span-4">
-                    <label htmlFor="to" className="block text-sm/6 font-medium text-gray-900">
-                        Wallet:
-                    </label>
-                    <div className="mt-2">
-                        <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                            <input
-                                type="text"
-                                id="to"
-                                name="to"
-                                value={to}
-                                placeholder="44FpkJSQUPYJjSYLKgg1qrt5PZAR9VRYFoaTJNbRXkCd"
-                                onChange={(e:ChangeEvent<HTMLInputElement>)=>{setTo(e.target.value)}}
-                                className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                <div>
+                    <h2 className="text-base/7 font-semibold text-gray-900">Send</h2>
+                    <p className="mt-1 text-sm/6 text-gray-600">
+                      Instantly transfer funds to another wallet in the same network.
+                    </p>
+                </div>
+
+
+
+
+                <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 md:col-span-2">
+
+
+                    <div className="sm:col-span-4">
+                        <label htmlFor="to" className="block text-sm/6 font-medium text-gray-900">
+                            Wallet:
+                        </label>
+                        <div className="mt-2">
+                            <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                                <input
+                                    type="text"
+                                    id="to"
+                                    name="to"
+                                    value={to}
+                                    placeholder="44FpkJSQUPYJjSYLKgg1qrt5PZAR9VRYFoaTJNbRXkCd"
+                                    onChange={(e:ChangeEvent<HTMLInputElement>)=>{setTo(e.target.value)}}
+                                    className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                                    required
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="sm:col-span-4">
+                        <label htmlFor="amount" className="block text-sm/6 font-medium text-gray-900">
+                            Amount:
+                        </label>
+                        <div className="mt-2">
+                            <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                                <input
+                                    type="number"
+                                    id="amount"
+                                    name="amount"
+                                    placeholder="100"
+                                    value={amount === 0 ? '' : amount}
+                                    onChange={handleInput}
+                                    className="uppercase block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                                    required
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="sm:col-span-4">
+                        <label htmlFor="memo" className="block text-sm/6 font-medium text-gray-900">
+                            Memo:
+                        </label>
+                        <div className="mt-2">
+                            <textarea
+                                id="memo"
+                                name="memo"
+                                rows={3}
+                                placeholder="Digital media licensing fees for Brain Farm Creative"
+                                value={memo}
+                                onChange={(e:ChangeEvent<HTMLTextAreaElement>)=>{setMemo(e.target.value)}}
+                                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                                 required
                             />
                         </div>
                     </div>
+
+
                 </div>
-
-
-                <div className="sm:col-span-4">
-                    <label htmlFor="amount" className="block text-sm/6 font-medium text-gray-900">
-                        Amount:
-                    </label>
-                    <div className="mt-2">
-                        <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                            <input
-                                type="number"
-                                id="amount"
-                                name="amount"
-                                placeholder="100"
-                                value={amount === 0 ? '' : amount}
-                                onChange={handleInput}
-                                className="uppercase block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
-                                required
-                            />
-                        </div>
-                    </div>
-                </div>
-
-
-                <div className="sm:col-span-4">
-                    <label htmlFor="memo" className="block text-sm/6 font-medium text-gray-900">
-                        Memo:
-                    </label>
-                    <div className="mt-2">
-                        <textarea
-                            id="memo"
-                            name="memo"
-                            rows={3}
-                            placeholder="Digital media licensing fees for Brain Farm Creative"
-                            value={memo}
-                            onChange={(e:ChangeEvent<HTMLTextAreaElement>)=>{setMemo(e.target.value)}}
-                            className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                            required
-                        />
-                    </div>
-                </div>
-
-
-
-              </div>
+            
+            
+            
+            
             </div>
 
 
             
-          </div>
+        </div>
     
 
 
-          <div className="mt-6 flex items-center justify-end gap-x-6">
-            <button
-              type="submit"
-              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer"
-            >
-              {loading ? 'Loading...' : 'Send'}
-            </button>
-          </div>
-          {error ? <p className="mt-6 flex items-center justify-end gap-x-6 text-red-400">{error}</p> : null}
+        <div className="mt-6 flex items-center justify-end gap-x-6">
+            {
+                signature ? <span className='text-md text-green-600'>Success!</span> :
+                <button
+                  type="submit"
+                  className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer"
+                >
+                  {loading ? 'Loading...' : 'Send'}
+                </button>
+            }
+        </div>
+        {error ? <p className="mt-6 flex items-center justify-end gap-x-6 text-red-400">{error}</p> : null}
 
 
 
