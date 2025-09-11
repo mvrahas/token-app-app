6b620f02951a3ad7e71f0cc148ec467973ac8e47
Mark Vrahas <mvrahas@users.noreply.github.com>
2025-09-11 14:18:03 -0700

    cleanup for settings screen

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -16,12 +16,20 @@ const Settings = () => {
         <div className="border-t border-gray-100 dark:border-white/5">
           <dl className="divide-y divide-gray-100 dark:divide-white/5">
             <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt className="text-sm font-medium text-gray-900 dark:text-gray-100">Organization</dt>
-              <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0 dark:text-gray-300">{organization ? organization.name : null}</dd>
+              <dt className="text-sm font-medium text-gray-900 dark:text-gray-100 flex flex-row items-center"><div>Organization</div></dt>
+              <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-1 sm:mt-0 dark:text-gray-300 flex flex-row items-center"><div>{organization ? organization.name : null}</div></dd>
+              <div className='sm:col-span-1 flex flex-row justify-end'>
+                <div
+                    onClick={()=>navigate('/settings/wallet')}
+                    className="invisible hidden rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50 sm:block cursor-pointer"
+                >
+                    Edit
+                </div>
+              </div>
             </div>
             <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt className="text-sm font-medium text-gray-900 dark:text-gray-100">Wallet</dt>
-              <dd className="mt-1 truncate text-sm/6 text-gray-700 sm:col-span-1 sm:mt-0 dark:text-gray-300">{organization ? organization.wallet : null}</dd>
+              <dt className="text-sm font-medium text-gray-900 dark:text-gray-100 flex flex-row items-center"><div>Wallet</div></dt>
+              <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-1 sm:mt-0 dark:text-gray-300 flex flex-row items-center"><div className='truncate'>{organization ? organization.wallet : null}</div></dd>
               <div className='sm:col-span-1 flex flex-row justify-end'>
                 <button
                     onClick={()=>navigate('/settings/wallet')}
@@ -32,8 +40,8 @@ const Settings = () => {
               </div>
             </div>
             <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt className="text-sm font-medium text-gray-900 dark:text-gray-100">API Keys</dt>
-              <dd className="mt-1 italic text-sm/6 text-gray-500 sm:col-span-1 sm:mt-0 dark:text-gray-300">Create & manage API keys</dd>
+              <dt className="text-sm font-medium text-gray-900 dark:text-gray-100 flex flex-row items-center"><div>API Keys</div></dt>
+              <dd className="mt-1 italic text-sm/6 text-gray-500 sm:col-span-1 sm:mt-0 dark:text-gray-300 flex flex-row items-center"><div>Create & manage API keys</div></dd>
               <div className='sm:col-span-1 flex flex-row justify-end'>
                 <button
                     onClick={()=>navigate('/settings/keys')}
